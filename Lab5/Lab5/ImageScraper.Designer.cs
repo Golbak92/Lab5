f32c5a55323b4a62d60a832c980b73a8bd000682
Kristian Golba <Golba.kristian@hotmail.com>
2020-11-09 00:20:13 +0100

    .

---
+++ Lab5/Lab5/ImageScraper.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Lab5
+{
+    partial class ImageScraper
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSaveImages = new System.Windows.Forms.Button();
+            this.textBoxURLInput = new System.Windows.Forms.TextBox();
+            this.buttonExtractImages = new System.Windows.Forms.Button();
+            this.labelNumberOfImages = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonSaveImages
+            // 
+            this.buttonSaveImages.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSaveImages.Location = new System.Drawing.Point(581, 422);
+            this.buttonSaveImages.Name = "buttonSaveImages";
+            this.buttonSaveImages.Size = new System.Drawing.Size(82, 20);
+            this.buttonSaveImages.TabIndex = 0;
+            this.buttonSaveImages.Text = "Save Images";
+            this.buttonSaveImages.UseVisualStyleBackColor = true;
+            this.buttonSaveImages.Click += new System.EventHandler(this.buttonSaveImages_Click);
+            // 
+            // textBoxURLInput
+            // 
+            this.textBoxURLInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxURLInput.Location = new System.Drawing.Point(12, 12);
+            this.textBoxURLInput.Name = "textBoxURLInput";
+            this.textBoxURLInput.Size = new System.Drawing.Size(563, 20);
+            this.textBoxURLInput.TabIndex = 1;
+            // 
+            // buttonExtractImages
+            // 
+            this.buttonExtractImages.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonExtractImages.Location = new System.Drawing.Point(581, 12);
+            this.buttonExtractImages.Name = "buttonExtractImages";
+            this.buttonExtractImages.Size = new System.Drawing.Size(82, 20);
+            this.buttonExtractImages.TabIndex = 2;
+            this.buttonExtractImages.Text = "Extract";
+            this.buttonExtractImages.UseVisualStyleBackColor = true;
+            this.buttonExtractImages.Click += new System.EventHandler(this.buttonExtractImageURLS_Click);
+            // 
+            // labelNumberOfImages
+            // 
+            this.labelNumberOfImages.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.labelNumberOfImages.AutoSize = true;
+            this.labelNumberOfImages.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNumberOfImages.Location = new System.Drawing.Point(12, 422);
+            this.labelNumberOfImages.Name = "labelNumberOfImages";
+            this.labelNumberOfImages.Size = new System.Drawing.Size(0, 19);
+            this.labelNumberOfImages.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 38);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(651, 381);
+            this.textBox1.TabIndex = 5;
+            // 
+            // ImageScraper
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(675, 454);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelNumberOfImages);
+            this.Controls.Add(this.buttonExtractImages);
+            this.Controls.Add(this.textBoxURLInput);
+            this.Controls.Add(this.buttonSaveImages);
+            this.Name = "ImageScraper";
+            this.Text = "ImageScraper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSaveImages;
+        private System.Windows.Forms.TextBox textBoxURLInput;
+        private System.Windows.Forms.Button buttonExtractImages;
+        private System.Windows.Forms.Label labelNumberOfImages;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Lab5/Lab5/ImageScraper.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab5
+{
+    public partial class ImageScraper : Form
+    {
+        private List<string> ImageURLs = new List<string>();
+        private Regex Pattern { get; set; }
+        private Regex FileExtension { get; set; }
+        private Dictionary<Task<byte[]>, string> Dictionary { get; set; } = new Dictionary<Task<byte[]>, string>();
+
+        public ImageScraper()
+        {
+            InitializeComponent();
+            Pattern = new Regex(@"(?<=<img.*src="")[^""]*");
+            FileExtension = new Regex(@"\.(png|jpg|jpeg|gif|bmp)");
+        }
+
+        private async void buttonSaveImages_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new FolderBrowserDialog())
+            {
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    await DownloadImages(dialog.SelectedPath);  
+                }
+            }
+        }
+
+        private async void buttonExtractImageURLS_Click(object sender, EventArgs e)
+        {
+            var input = textBoxURLInput.Text;
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return;
+            }
+
+            var urlInputTask = ExtractImages("http://www." + input);
+            await urlInputTask;
+
+            if (!input.Contains("http://"))
+            {
+                input = $"http://{input}";
+            }
+
+            var matches = Pattern.Matches(urlInputTask.Result);
+
+            for (int i = 0; i < matches.Count; i++)
+            {
+                var result = "";
+
+                if (matches[i].Value.Contains("http"))
+                {
+                    result = matches[i].Value + Environment.NewLine;
+                }
+                else if(!string.IsNullOrWhiteSpace(matches[i].Value))
+                {
+                    result = "gp.se" + matches[i] + Environment.NewLine;
+                }
+                ImageURLs.Add(result);
+                textBox1.Text += result;
+
+                labelNumberOfImages.Text = "Found " + ImageURLs.Count.ToString() + " images.";
+            }
+        }
+
+        private async Task<string> ExtractImages(string url)
+        {
+            HttpClient client = new HttpClient();
+            var urlInputResult = client.GetStringAsync(url);
+
+            await urlInputResult;
+
+            return urlInputResult.Result;
+        }
+
+
+        private async Task DownloadImages(string path)
+        {
+            using var client = new HttpClient();
+            var imageArray = ImageURLs.ToArray();
+
+            foreach (var image in imageArray)
+            {
+                var match = FileExtension.Matches(image);
+                Dictionary.Add(client.GetByteArrayAsync(image), match[0].Value);
+            }
+
+            var i = 1;
+            var tasks = Dictionary.Keys;
+            while (Dictionary.Count > 0)
+            {
+                var finishedTasks = await Task.WhenAny(tasks);
+                var extension = Dictionary[finishedTasks];
+                var result = finishedTasks.Result;
+
+                var fileSream = new FileStream($"{path}\\image{i}{extension}", FileMode.Create);
+                i++;
+                await fileSream.WriteAsync(result, 0, result.Length);
+
+                Dictionary.Remove(finishedTasks);
+            }
+        }
+    }
+}
